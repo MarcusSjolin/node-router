fab7256fe25bf336601266db3f8b1010cb1a8c8d
Marcus Sjolin <marcus.sjolin@ticketmaster.co.uk>
2015-08-24 14:41:20 +0100

    Removed some testing code

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
 exports.Router = require("./src/Router/Router")
 exports.Route = require("./src/Route/Route")
-
-
-var myRouter = this.Router()
-
-myRouter.add("marcus", "/marcus/:id/:date")
-console.log(myRouter.find("/marcus/a/a"))
